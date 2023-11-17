644d345f3a6c12e8163711df301864817e09e5e4
Prince K Niyon <princekungu8@gmail.com>
2023-11-17 16:32:41 +0300

    login 2

--- script.js
+++ script.js
@@ -79,3 +79,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Both username and password are required.");
+    } else {
+        // Redirect to index.html
+        window.location.href = "index.html";
+    }
+}
+
